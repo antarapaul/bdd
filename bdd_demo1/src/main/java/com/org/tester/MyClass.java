409a235d1ca2e9a4aba8b7865d11460939b799fb
antpaul <antpaul@DIN73003043.corp.capgemini.com>
2018-11-23 14:12:19 +0530

    fist

---
+++ bdd_demo1/src/main/java/com/org/tester/MyClass.java
@@ -0,0 +1,15 @@
+package com.org.tester;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("In main..");
+		
+		MyMethod();
+	}
+	
+	public static void MyMethod() {
+	System.out.println("In my method..");
+	}
+
+}
